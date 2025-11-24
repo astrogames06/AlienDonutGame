4059aea2062d4656cc1eb7a24fa6f496e323d3e1
Astro <88717385+MilkMouseBearCoder@users.noreply.github.com>
2025-11-25 08:03:20 +1100

    bascially finished game but havent tested

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,51 @@
     #include <emscripten/emscripten.h>
 #endif
 
-
 const int WIDTH = 850;
 const int HEIGHT = 450;
+int score = 0;
+
+std::vector<std::pair<Texture2D*, Vector2>> donuts;
+Texture2D alien_invasion;
+Texture2D asteroid_crumbcake;
+Texture2D quantum_foam;
+Texture2D solar_flare;
+Texture2D titan_toffee;
+Texture2D venusian_volcano;
+std::vector<Texture2D*> donut_selection;
+
+int donut_adding_timer = 0;
+
+Texture2D player_tex;
+float player_x;
+float player_y;
 
 void UpdateDrawFrame();
 
 int main(void)
 {
 	InitWindow(WIDTH, HEIGHT, "raylib [core] example - basic window");
+	player_tex = LoadTexture("images/alien-donut-eater.png");
+	player_x = WIDTH/2-player_tex.width/2;
+	player_y = HEIGHT-player_tex.height-20;
+	player_tex.width /= 4;
+	player_tex.height /= 4;
+
+	alien_invasion = LoadTexture("images/alien-invasion.png");
+	asteroid_crumbcake = LoadTexture("images/asteroid-crumbcake.png");
+	quantum_foam = LoadTexture("images/quantum-foam.png");
+	solar_flare = LoadTexture("images/solar-flare.png");
+	titan_toffee = LoadTexture("images/titan-toffee.png");
+	venusian_volcano = LoadTexture("images/venusian-volcano.png");
+
+	donut_selection = {
+		&alien_invasion, &asteroid_crumbcake, &quantum_foam,
+		&solar_flare, &titan_toffee, &venusian_volcano
+	};
+	for (Texture2D* tex : donut_selection)
+	{
+		tex->width /= 10; tex->height /= 10;
+	}
 
 	#if defined(PLATFORM_WEB)
     	emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
@@ -43,16 +79,37 @@ int main(void)
 
 void UpdateDrawFrame()
 {
+	donut_adding_timer++;
+
+	if (donut_adding_timer >= GetFPS())
+	{
+		donuts.push_back(std::make_pair(
+			donut_selection[GetRandomValue(0, donut_selection.size())],
+			Vector2 {(float)GetRandomValue(0, WIDTH), -80}
+		));
+	}
 	BeginDrawing();
 	
-	ClearBackground(RAYWHITE);
+	ClearBackground(DARKPURPLE);
 
-	DrawText("Raylib Game template!", 100, 100, 20, BLACK);
-
-	if (GuiButton({200, 200, 100, 30}, "#32# PRESS ME NOW!"))
+	for (int i = 0; i < donuts.size(); i++)
 	{
-		std::cout << "Pressed!\n";
+		donuts[i].second.y -= 100 * GetFrameTime();
+		if (CheckCollisionRecs(
+			{player_x, player_y, (float)player_tex.width, (float)player_tex.height},
+			{donuts[i].second.x, donuts[i].second.y, (float)donuts[i].first->width, (float)donuts[i].first->height}
+		))
+		{
+			donuts.erase(donuts.begin() + i);
+			score++;
+		}
+
+		DrawTextureV(*donuts[i].first, donuts[i].second, WHITE);
 	}
 
+	DrawTexture(player_tex, player_x, player_y, WHITE);
+
+	std::string score_str = "Score: " + score;
+	DrawText(score_str.c_str(), 20, 20, 20, WHITE);
 	EndDrawing();
 }
